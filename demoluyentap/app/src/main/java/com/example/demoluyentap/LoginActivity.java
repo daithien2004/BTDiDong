d61ee77f0bc0fb3f9496d22face05cfdeb70d6a0
DTCode <daithien2004@gmail.com>
2025-02-21 09:52:27 +0700

    Thêm sự kiện onclick cho màn hình login, signup

--- demoluyentap/app/src/main/java/com/example/demoluyentap/LoginActivity.java
+++ demoluyentap/app/src/main/java/com/example/demoluyentap/LoginActivity.java
@@ -1,15 +1,28 @@
 package com.example.demoluyentap;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+
+        imageView = findViewById(R.id.imageView2);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- demoluyentap/app/src/main/java/com/example/demoluyentap/SignupActivity.java
+++ demoluyentap/app/src/main/java/com/example/demoluyentap/SignupActivity.java
@@ -1,15 +1,28 @@
 package com.example.demoluyentap;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignupActivity extends AppCompatActivity {
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_signup);
+
+        imageView = findViewById(R.id.imageView2);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
